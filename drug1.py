0da2276a219c98ae18fa7d2adf1cb72bfbabb302
justpcode89 <justpcode89@gmail.com>
2024-11-05 09:32:30 +0330

    Create drug1.py

---
+++ drug1.py
@@ -0,0 +1,10 @@
+drug_data = [
+    {"Drug Name": "Aspirin", "Molecular Weight": 180.16, "LogP": 1.2, "Bioavailability": 68, "Therapeutic Class": "Analgesic"},
+    {"Drug Name": "Metformin", "Molecular Weight": 129.16, "LogP": -1.43, "Bioavailability": 50, "Therapeutic Class": "Antidiabetic"},
+    {"Drug Name": "Ibuprofen", "Molecular Weight": 206.29, "LogP": 3.97, "Bioavailability": 80, "Therapeutic Class": "Anti-inflammatory"},
+    {"Drug Name": "Atorvastatin", "Molecular Weight": 558.64, "LogP": 5.7, "Bioavailability": 12, "Therapeutic Class": "Antihyperlipidemic"},
+    {"Drug Name": "Paracetamol", "Molecular Weight": 151.16, "LogP": 0.46, "Bioavailability": 85, "Therapeutic Class": "Analgesic"},
+    {"Drug Name": "Ciprofloxacin", "Molecular Weight": 331.34, "LogP": -0.6, "Bioavailability": 70, "Therapeutic Class": "Antibiotic"},
+    {"Drug Name": "Amoxicillin", "Molecular Weight": 365.4, "LogP": -0.8, "Bioavailability": 95, "Therapeutic Class": "Antibiotic"},
+    {"Drug Name": "Omeprazole", "Molecular Weight": 345.42, "LogP": 2.3, "Bioavailability": 50, "Therapeutic Class": "Proton Pump Inhibitor"}
+]
